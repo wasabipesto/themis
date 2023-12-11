35c1eeda913ae697648f2bbd23b0cd7c93c1dc77
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-12-11 15:59:42 -0500

    basic fetch cli

--- themis-fetch/src/main.rs
+++ themis-fetch/src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Parser, ValueEnum};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Override the default platform list to only pull from one provider
+    #[arg(short, long)]
+    platform: Option<Platform>,
+
+    /// Only pull market data for a single market - requires a single platform to be specified
+    #[arg(long)]
+    id: Option<String>,
+
+    /// Redirect output to the database ["db"], the console ["stdout"], or a file [value used as filename]
+    #[arg(short, long, default_value = "db")]
+    output: String,
+
+    /// Show additional output for debugging
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Platform {
+    Kalshi,
+    Manifold,
+    Metaculus,
+    Polymarket,
+    PredictIt,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    // get platform list
+    let platforms: Vec<&Platform> = match &args.platform {
+        Some(platform) => Vec::from([platform]),
+        None => Vec::from([&Platform::Kalshi, &Platform::Manifold]),
+    };
+    println!("Getting data from {:?}", &platforms)
 }
