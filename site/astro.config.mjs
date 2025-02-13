cf536ea381af156568d0f5bf4c8ed9d591f2b5c8
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-02-13 18:50:53 -0500

    add a few site basics

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://predictionmetrics.org",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
