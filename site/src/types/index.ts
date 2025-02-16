c4047d52ddc86da06754a983cc75053eb2f80dff
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-02-16 11:05:48 -0500

    add basic pm explainer

---
+++ site/src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Platform {
+  name_lowercase: string;
+  name_display: string;
+  slug: string;
+  description: string;
+  avatar_url: string;
+  site_url: string;
+  color_primary: string;
+  color_accent: string;
+}
