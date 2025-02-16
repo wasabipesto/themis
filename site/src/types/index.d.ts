fd02c2b4693cf0573a7326ee50f5123aa5554473
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-02-16 17:50:55 -0500

    various formatting updates

--- site/src/types/index.d.ts
+++ site/src/types/index.d.ts
@@ -1,7 +1,7 @@
+/** All data for a prediction market platform. */
 export interface Platform {
-  name_lowercase: string;
-  name_display: string;
   slug: string;
+  name: string;
   description: string;
   icon_url: string;
   site_url: string;
@@ -9,55 +9,78 @@ export interface Platform {
   color_accent: string;
 }
 
-export interface Category {
-  name_display: string;
-  slug: string;
-  parent_slug: string | null;
-  description: string;
-  icon_url: string;
+/** Score data for a platform's performance within a category, or overall. */
+export interface PlatformScore {
+  platform_slug: string;
+  platform_name: string;
+  category_slug: string;
+  category_name: string;
+  num_markets: number;
+  grade: string;
+  score_rel: number;
+  score_abs: number;
 }
 
+/**
+ * All data for a category (a collection of questions).
+ * Can be a parent or a child category.
+ */
+export interface Category {
+  slug: string;
+  name: string;
+  description: string;
+  parent_slug: string | null;
+  is_parent: boolean;
+  icon_url: string;
+  total_markets: number;
+  total_traders: number;
+  total_volume: number;
+}
+
+/** All data for a question (a group of markets). */
 export interface Question {
-  questionId: string;
+  question_id: string;
   title: string;
   slug: string;
   description: string;
-  parentCategory: string;
-  subCategory: string | null;
+  category_slug: string;
+  category_name: string;
+  parent_category_slug: string | null;
+  parent_category_name: string | null;
   tags: string[];
-  totalTraders: number;
-  totalVolume: number;
-  totalDuration: number;
-  startDate: string;
-  endDate: string;
-  markets: ScoredMarket[];
-  overall: ScoreOverall;
+  start_date: string;
+  end_date: string;
+  total_traders: number;
+  total_volume: number;
+  total_duration: number;
+  overall_grade: string;
+  overall_score_rel: number;
+  overall_score_abs: number;
+  markets: QMarketScore[];
 }
 
-type ScoredMarket = {
-  platform: string;
-  marketId: string;
-  link: string;
+/** Score data for a market's performance within a question. */
+type QMarketScore = {
+  platform_slug: string;
+  platform_name: string;
+  market_id: string;
+  market_link: string;
   traders: number | null;
   volume: number | null;
   duration: number;
   grade: string;
-  scoreRel: number;
-  scoreAbs: number;
+  score_rel: number;
+  score_abs: number;
 };
 
-type ScoreOverall = {
-  grade: string;
-  scoreRel: number;
-  scoreAbs: number;
-};
-
+/** Single point on a daily probability plot. */
 export interface DailyProbability {
   platform: string;
   date: string;
   prob: number;
 }
 
+/** Single point on a caliibration plot. */
 export interface CalibrationPoint {
   platform: string;
   x_start: number | null;
