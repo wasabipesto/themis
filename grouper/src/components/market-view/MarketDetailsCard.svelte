a90a61a6011aca68224da9808d56d748267021db
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-03-30 18:48:30 -0400

    group: traders icon on market chips

--- grouper/src/components/market-view/MarketDetailsCard.svelte
+++ grouper/src/components/market-view/MarketDetailsCard.svelte
@@ -67,6 +67,18 @@
     {#if market.traders_count}
       <span class="text-sm bg-blue/20 text-text px-4 py-1 rounded-md mr-2 mb-2">
         {market.traders_count?.toLocaleString() || "N/A"}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          height={16}
+          fill="currentColor"
+          class="inline mx-0.5 mr-1"
+        >
+          <title>People</title>
+          <path
+            d="M16 17V19H2V17S2 13 9 13 16 17 16 17M12.5 7.5A3.5 3.5 0 1 0 9 11A3.5 3.5 0 0 0 12.5 7.5M15.94 13A5.32 5.32 0 0 1 18 17V19H22V17S22 13.37 15.94 13M15 4A3.39 3.39 0 0 0 13.07 4.59A5 5 0 0 1 13.07 10.41A3.39 3.39 0 0 0 15 11A3.5 3.5 0 0 0 15 4Z"
+          />
+        </svg>
       </span>
     {/if}
     <span class="text-sm bg-blue/20 text-text px-4 py-1 rounded-md mr-2 mb-2">
