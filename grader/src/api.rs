248c4213c4ea23ec01da0684eb1a39b8886a9f2a
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-04-08 22:08:52 -0400

    grade: add more api calls and refactor

--- grader/src/api.rs
+++ grader/src/api.rs
@@ -1,9 +1,13 @@
 //! Module containing common API functions.
 
-use crate::{scores::MarketScore, DailyProbabilityPoint, Question};
+use crate::{
+    scores::{MarketScore, OtherScore, PlatformCategoryScore},
+    Category, DailyProbabilityPoint, Platform, Question,
+};
 
 use super::PostgrestParams;
 use anyhow::{Context, Result};
+use log::debug;
 use reqwest::blocking::{Client, Response};
 use std::{fmt::Display, time::Duration};
 
@@ -25,27 +29,79 @@ impl Display for HttpMethod {
     }
 }
 
+/// Make a simple get request
+fn make_get_request<T>(client: &Client, params: &PostgrestParams, endpoint: &str) -> Result<T>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let response = make_request(
+        client,
+        params,
+        endpoint,
+        HttpMethod::GET,
+        Option::<&()>::None,
+    )?;
+    process_response(response, endpoint)
+}
+
+/// Make a simple post request
+fn make_post_request(
+    client: &Client,
+    params: &PostgrestParams,
+    endpoint: &str,
+    body: Option<&impl serde::Serialize>,
+) -> Result<()> {
+    let response = make_request(client, params, endpoint, HttpMethod::POST, body)?;
+    process_empty_response(response, endpoint)
+}
+
+/// Make a simple delete request
+fn make_delete_request(client: &Client, params: &PostgrestParams, endpoint: &str) -> Result<()> {
+    let response = make_request(
+        client,
+        params,
+        endpoint,
+        HttpMethod::DELETE,
+        Option::<&()>::None,
+    )?;
+    process_empty_response(response, endpoint)
+}
+
 /// Makes an API request with the specified method and handles errors consistently
 fn make_request(
     client: &Client,
-    url: String,
+    params: &PostgrestParams,
+    endpoint: &str,
     method: HttpMethod,
-    auth_key: &str,
+    body: Option<&impl serde::Serialize>,
 ) -> Result<Response> {
-    let request_builder = match method {
-        HttpMethod::GET => client.get(&url),
-        HttpMethod::POST => client.post(&url),
-        HttpMethod::DELETE => client.delete(&url),
+    // Build the URL
+    let url = format!("{}{}", params.postgrest_url, endpoint);
+    debug!("Sending a {method} request to {url}");
+
+    // Set the correct method
+    let mut request_builder = match method {
+        HttpMethod::GET => client.get(url),
+        HttpMethod::POST => client.post(url),
+        HttpMethod::DELETE => client.delete(url),
     };
 
+    // Add bearer auth
+    request_builder = request_builder.bearer_auth(&params.postgrest_api_key);
+
+    // Add the body to the request if provided
+    if let Some(data) = body {
+        request_builder = request_builder.json(data);
+    }
+
+    // Ship it
     request_builder
-        .bearer_auth(auth_key)
         .send()
-        .with_context(|| format!("Failed to send {method} request to {url}"))
+        .with_context(|| format!("Failed to send {method} request to {endpoint}"))
 }
 
 /// Process API response, returning deserialized data or an error
-fn process_response<T>(response: Response, error_context: &str) -> Result<T>
+fn process_response<T>(response: Response, endpoint: &str) -> Result<T>
 where
     T: serde::de::DeserializeOwned,
 {
@@ -57,26 +113,20 @@ where
     } else {
         let body = response.text()?;
         Err(anyhow::anyhow!(
-            "{} failed with status {} and body: {}",
-            error_context,
-            status,
-            body
+            "Request to {endpoint} failed with status {status} and body: {body}",
         ))
     }
 }
 
 /// Process a response when we don't need the body content, just success/failure
-fn process_empty_response(response: Response, error_context: &str) -> Result<()> {
+fn process_empty_response(response: Response, endpoint: &str) -> Result<()> {
     let status = response.status();
     if status.is_success() {
         Ok(())
     } else {
         let body = response.text()?;
         Err(anyhow::anyhow!(
-            "{} failed with status {} and body: {}",
-            error_context,
-            status,
-            body
+            "Request to {endpoint} failed with status {status} and body: {body}",
         ))
     }
 }
@@ -84,23 +134,19 @@ fn process_empty_response(response: Response, error_context: &str) -> Result<()>
 /// Downloads all markets from the database.
 /// Paginates through the PostgREST endpoint until all are downloaded.
 pub fn get_all_markets(client: &Client, params: &PostgrestParams) -> Result<Vec<Market>> {
-    let limit = 1000;
+    let limit = 10000;
     let mut offset = 0;
     let mut markets = Vec::new();
 
     loop {
-        let url = format!(
-            "{}/market_details?order=id&limit={}&offset={}",
-            params.postgrest_url, limit, offset
+        let endpoint = format!(
+            "/market_details?order=id.asc&limit={}&offset={}",
+            limit, offset
         );
-
-        let response = make_request(client, url, HttpMethod::GET, &params.postgrest_api_key)?;
-        let body: Vec<Market> = process_response(response, "Download markets")?;
-
+        let body: Vec<Market> = make_get_request(client, params, &endpoint)?;
         if body.is_empty() {
             break;
         }
-
         markets.extend(body);
         offset += limit;
     }
@@ -108,29 +154,22 @@ pub fn get_all_markets(client: &Client, params: &PostgrestParams) -> Result<Vec<
     Ok(markets)
 }
 
+/// Downloads all platforms from the database.
+pub fn get_all_platforms(client: &Client, params: &PostgrestParams) -> Result<Vec<Platform>> {
+    let response: Vec<Platform> = make_get_request(client, params, "/platform_details")?;
+    Ok(response)
+}
+
+/// Downloads all categories from the database.
+pub fn get_all_categories(client: &Client, params: &PostgrestParams) -> Result<Vec<Category>> {
+    let response: Vec<Category> = make_get_request(client, params, "/category_details")?;
+    Ok(response)
+}
+
 /// Downloads all requested questions by ID.
-pub fn get_questions(
-    client: &Client,
-    params: &PostgrestParams,
-    question_ids: &[u32],
-) -> Result<Vec<Question>> {
-    let mut questions = Vec::with_capacity(question_ids.len());
-
-    for question_id in question_ids {
-        let url = format!(
-            "{}/question_details?id=eq.{}",
-            params.postgrest_url, question_id
-        );
-
-        let response = make_request(client, url, HttpMethod::GET, &params.postgrest_api_key)?;
-        let body: Vec<Question> = process_response(response, "Download questions")?;
-
-        if let Some(question) = body.first() {
-            questions.push(question.clone());
-        }
-    }
-
-    Ok(questions)
+pub fn get_questions(client: &Client, params: &PostgrestParams) -> Result<Vec<Question>> {
+    let response: Vec<Question> = make_get_request(client, params, "/question_details")?;
+    Ok(response)
 }
 
 /// Downloads all requested probability points by market ID.
@@ -142,16 +181,12 @@ pub fn get_market_probs(
     let mut probs = Vec::new();
 
     for market_id in market_ids {
-        let url = format!(
-            "{}/daily_probabilities?order=date&market_id=eq.{}",
-            params.postgrest_url, market_id
+        let endpoint = format!(
+            "/daily_probabilities?order=date.asc&market_id=eq.{}",
+            market_id
         );
-
-        let response = make_request(client, url, HttpMethod::GET, &params.postgrest_api_key)?;
-        let body: Vec<DailyProbabilityPoint> =
-            process_response(response, "Download probabilities")?;
-
-        probs.extend(body);
+        let response: Vec<DailyProbabilityPoint> = make_get_request(client, params, &endpoint)?;
+        probs.extend(response);
     }
 
     Ok(probs)
@@ -159,38 +194,54 @@ pub fn get_market_probs(
 
 /// Wipes all market scores from the database.
 pub fn wipe_market_scores(client: &Client, params: &PostgrestParams) -> Result<()> {
-    let url = format!("{}/market_scores", params.postgrest_url);
-
-    let response = make_request(client, url, HttpMethod::DELETE, &params.postgrest_api_key)?;
-    process_empty_response(response, "Wipe market scores")
+    make_delete_request(client, params, "/market_scores")
 }
 
 /// Uploads market scores to the database.
 pub fn upload_market_scores(
     client: &Client,
     params: &PostgrestParams,
-    scores: &[MarketScore],
+    scores: &Vec<MarketScore>,
 ) -> Result<()> {
-    let url = format!("{}/market_scores", params.postgrest_url);
+    make_post_request(client, params, "/market_scores", Some(scores))
+}
 
-    let response = client
-        .post(&url)
-        .bearer_auth(&params.postgrest_api_key)
-        .json(scores)
-        .send()
-        .with_context(|| format!("Failed to send POST request to {url}"))?;
-    process_empty_response(response, "Wipe market scores")
+/// Wipes all platform-category scores from the database.
+pub fn wipe_platform_category_scores(client: &Client, params: &PostgrestParams) -> Result<()> {
+    make_delete_request(client, params, "/platform_category_scores")
+}
+
+/// Uploads platform-category scores to the database.
+pub fn upload_platform_category_scores(
+    client: &Client,
+    params: &PostgrestParams,
+    scores: &Vec<PlatformCategoryScore>,
+) -> Result<()> {
+    make_post_request(client, params, "/platform_category_scores", Some(scores))
+}
+
+/// Wipes all other scores from the database.
+pub fn wipe_other_scores(client: &Client, params: &PostgrestParams) -> Result<()> {
+    make_delete_request(client, params, "/other_scores")
+}
+
+/// Uploads other scores to the database.
+pub fn upload_other_scores(
+    client: &Client,
+    params: &PostgrestParams,
+    scores: &Vec<OtherScore>,
+) -> Result<()> {
+    make_post_request(client, params, "/other_scores", Some(scores))
 }
 
 /// Refreshes the market and question materialized views in the database.
 pub fn refresh_quick_materialized_views(client: &Client, params: &PostgrestParams) -> Result<()> {
-    let url = format!(
-        "{}/rpc/refresh_quick_materialized_views",
-        params.postgrest_url
-    );
-
-    let response = make_request(client, url, HttpMethod::POST, &params.postgrest_api_key)?;
-    process_empty_response(response, "Refresh quick materialized views")
+    make_post_request(
+        client,
+        params,
+        "/rpc/refresh_quick_materialized_views",
+        Option::<&()>::None,
+    )
 }
 
 /// Refreshes all materialized views in the database.
@@ -199,21 +250,15 @@ pub fn refresh_quick_materialized_views(client: &Client, params: &PostgrestParam
 pub fn refresh_all_materialized_views(params: &PostgrestParams) -> Result<()> {
     // Create a new client with a longer timeout specifically for this operation
     let timeout = Duration::from_secs(180); // 3 minute timeout
-    let long_timeout_client = Client::builder()
+    let client = Client::builder()
         .timeout(timeout)
         .build()
         .context("Failed to create HTTP client with extended timeout")?;
 
-    let url = format!(
-        "{}/rpc/refresh_all_materialized_views",
-        params.postgrest_url
-    );
-
-    let response = make_request(
-        &long_timeout_client,
-        url,
-        HttpMethod::POST,
-        &params.postgrest_api_key,
-    )?;
-    process_empty_response(response, "Refresh all materialized views")
+    make_post_request(
+        &client,
+        params,
+        "/rpc/refresh_all_materialized_views",
+        Option::<&()>::None,
+    )
 }
