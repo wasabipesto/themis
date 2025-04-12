a0341c8e1d3535b7b2d86047dcd8c8ce60c01ecf
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-04-12 13:32:26 -0400

    grade: refine relative score cutoffs

--- grader/src/scores/logarithmic.rs
+++ grader/src/scores/logarithmic.rs
@@ -37,21 +37,20 @@ pub fn abs_log_letter_grade(score: &f32) -> String {
 /// Convert a Logarithmic score to a letter grade.
 pub fn rel_log_letter_grade(score: &f32) -> String {
     match score {
-        x if *x < -0.95 => "S".to_string(),
-        x if *x < -0.75 => "A+".to_string(),
-        x if *x < -0.55 => "A".to_string(),
-        x if *x < -0.35 => "A-".to_string(),
-        x if *x < -0.25 => "B+".to_string(),
-        x if *x < -0.15 => "B".to_string(),
-        x if *x < -0.05 => "B-".to_string(),
-        x if *x < 0.0 => "C+".to_string(),
-        x if *x < 0.05 => "C".to_string(),
-        x if *x < 0.15 => "C-".to_string(),
-        x if *x < 0.25 => "D+".to_string(),
-        x if *x < 0.35 => "D".to_string(),
-        x if *x < 0.55 => "D-".to_string(),
-        x if *x <= 1.0 => "F".to_string(),
-        _ => "ERROR".to_string(),
+        x if *x > 0.200 => "S".to_string(),
+        x if *x > 0.060 => "A+".to_string(),
+        x if *x > 0.030 => "A".to_string(),
+        x if *x > 0.015 => "A-".to_string(),
+        x if *x > 0.010 => "B+".to_string(),
+        x if *x > 0.004 => "B".to_string(),
+        x if *x > 0.002 => "B-".to_string(),
+        x if *x > 0.000 => "C+".to_string(),
+        x if *x > -0.004 => "C".to_string(),
+        x if *x > -0.006 => "C-".to_string(),
+        x if *x > -0.010 => "D+".to_string(),
+        x if *x > -0.040 => "D".to_string(),
+        x if *x > -0.085 => "D-".to_string(),
+        _ => "F".to_string(),
     }
 }
 
