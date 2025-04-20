8647636fcf5695db02db31b89968e9aef5266687
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-04-20 17:21:48 -0400

    site: calculate some stats rather than defining data range

---
+++ site/src/lib/stats.ts
@@ -0,0 +1,68 @@
+// Takes an array of numbers and a percentile value
+// Returns the value at that percentile of the array
+// Assumes the scores are pre-sorted
+export function percentile(arr: number[], percentile: number): number {
+  const index = percentile * (arr.length - 1);
+
+  if (Number.isInteger(index)) {
+    return arr[index];
+  } else {
+    // interpolate to get percentile between points
+    const lowerIndex = Math.floor(index);
+    const upperIndex = Math.ceil(index);
+    return (
+      arr[lowerIndex] +
+      (index - lowerIndex) * (arr[upperIndex] - arr[lowerIndex])
+    );
+  }
+}
+
+// Takes an array of numbers and returns some stats
+// Assumes the scores are pre-sorted
+export function quartiles(arr: number[]): {
+  min: number;
+  c1: number;
+  q1: number;
+  q2: number;
+  q3: number;
+  c3: number;
+  c9: number;
+  max: number;
+  iqr: number;
+} {
+  const min = arr[0];
+  const q1 = percentile(arr, 0.25);
+  const q2 = percentile(arr, 0.5);
+  const q3 = percentile(arr, 0.75);
+  const max = arr[arr.length - 1];
+  const iqr = q3 - q1;
+  const c1 = Math.max(q1 - 1.5 * iqr, min);
+  const c3 = Math.min(q3 + 1.5 * iqr, max);
+  const c9 = percentile(arr, 0.975);
+  return {
+    min,
+    c1,
+    q1,
+    q2,
+    q3,
+    c3,
+    c9,
+    max,
+    iqr,
+  };
+}
+
+// Takes an array of numbers and sorts them
+export function sort(arr: number[]): number[] {
+  return arr.sort((a, b) => a - b);
+}
+
+// Rounds a number to n significant figures
+// Only necessary to work with numbers > 1
+export function roundSF(num: number, sigfigs: number): number {
+  const magnitude = Math.floor(Math.log10(Math.abs(num)));
+  const scale = Math.pow(10, sigfigs - magnitude - 1);
+  const roundedNum = Math.round(num * scale) / scale;
+
+  return roundedNum;
+}
